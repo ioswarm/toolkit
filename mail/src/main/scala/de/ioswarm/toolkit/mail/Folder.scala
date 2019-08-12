d1d4e55e718f8c780be25187dd26dc19f95d06e4
Andreas Pape <andreas.pape@euroleasing.eu>
2019-08-12 11:26:21 +0200

    remove private - append methods for name, fullname and list child-folder

--- mail/src/main/scala/de/ioswarm/toolkit/mail/Folder.scala
+++ mail/src/main/scala/de/ioswarm/toolkit/mail/Folder.scala
@@ -5,7 +5,10 @@ import javax.mail.{Folder => MailFolder}
 /**
   * Created by apape on 20.11.17.
   */
-private[mail] class Folder(mailFolder: MailFolder) {
+class Folder(val mailFolder: MailFolder) {
+
+  def name: String = mailFolder.getName
+  def fullName: String = mailFolder.getFullName
 
   def printMails(): Unit = {
     if (!mailFolder.isOpen) open()
@@ -27,5 +30,7 @@ private[mail] class Folder(mailFolder: MailFolder) {
     if (!mailFolder.isOpen) open()
     mailFolder.getMessages.map(m => new MessageMailImpl(m))
   }
+
+  def list:Array[Folder] = mailFolder.list().map(new Folder(_))
 
 }
